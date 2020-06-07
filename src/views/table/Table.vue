760c69d7a90962b3f518b617b4b7d6cb98ee1547
d_y_chao <dai.ych@neusoft.com>
2020-06-07 11:28:28 +0800

    1.表格页面新增dialog、form组件。

--- src/views/table/Table.vue
+++ src/views/table/Table.vue
@@ -46,6 +46,31 @@
             @size-change="handleSizeChange"
             @current-change="handleCurrentChange">
         </el-pagination>
+        <el-dialog
+            title="编辑"
+            :visible.sync="dialogVisible"
+            width="400px"
+            center>
+            <el-form ref="validateForm" :model="selectedObj" :rules="rules" label-width="80px">
+                <el-form-item label="姓名" prop="name">
+                    <el-input v-model="selectedObj.name"></el-input>
+                </el-form-item>
+                <el-form-item label="性别" prop="sex">
+                    <el-radio-group v-model="selectedObj.sex">
+                        <el-radio label="男"></el-radio>
+                        <el-radio label="女"></el-radio>
+                    </el-radio-group>
+                </el-form-item>
+                <el-form-item label="年龄" prop="age">
+                    <el-input v-model.number="selectedObj.age"></el-input>
+                </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="submitForm('validateForm')">确 定</el-button>
+            </span>
+        </el-dialog>
+
     </div>
 </template>
 
@@ -93,7 +118,26 @@ export default {
                 text: '男', value: '男'
             }, {
                 text: '女', value: '女'
-            }]
+            }],
+            dialogVisible: false,
+            selectedObj: {
+                name: '',
+                sex: '',
+                age: ''
+            },
+            selectedIndex: 0,
+            rules: {
+                name: [
+                    {required: true, message: '请输入活动名称', trigger: 'blur'}
+                ],
+                sex: [
+                    {required: true, message: '请选择性别', trigger: 'blur'}
+                ],
+                age: [
+                    {required: true, message: '年龄不能为空'},
+                    {type: 'number', message: '年龄必须为数字值'}
+                ]
+            }
         };
     },
     computed: {
@@ -115,7 +159,9 @@ export default {
             return row[property] === value;
         },
         handleEdit(index, row) {
-            console.log(index, row);
+            this.selectedObj = Object.assign({}, row);
+            this.selectedIndex = index;
+            this.dialogVisible = true;
         },
         handleDelete(index, row) {
             console.log(index, row);
@@ -130,6 +176,21 @@ export default {
         },
         indexMethod(index) { // 序号
             return index + (this.pageNum - 1) * this.pageSize + 1;
+        },
+        submitForm(validateForm) { // 保存
+            this.$refs[validateForm].validate((valid) => {
+                if (valid) {
+                    this.$message({
+                        message: '保存成功',
+                        type: 'success'
+                    });
+                    this.tableData[this.selectedIndex] = Object.assign({}, this.selectedObj);
+                    this.$set(this.tableData, this.selectedIndex, Object.assign({}, this.selectedObj));
+                    this.dialogVisible = false;
+                } else {
+                    return false;
+                }
+            });
         }
     }
 };
