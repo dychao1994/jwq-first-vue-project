10d44ae4fc18722fd996533fb6744b8871b8586d
d_y_chao <dai.ych@neusoft.com>
2020-10-13 22:25:46 +0800

    1.完善六爻组件。

--- src/views/liuYao/components/Gua.vue
+++ src/views/liuYao/components/Gua.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="gua">
-        <!-- 本卦 -->
+        <!-- 六神 -->
+        <div class="liu-shen">
+            <div class="gua-title">六神</div>
+            <div v-for="(item, index) in gua.liuShen" :key="index" class="single-row">
+                {{item}}
+            </div>
+        </div><!-- 本卦 -->
         <div class="ben-gua">
             <div class="gua-title">本卦</div>
             <div v-for="(item, index) in gua.benGua.replace(/(.)(?=[^$])/g,'$1,').split(',')" :key="index">
@@ -26,7 +32,7 @@ export default {
     data() {
         return {
             gua: {
-                liuShen: [],
+                liuShen: ['勾陈', '朱雀', '青龙', '玄武', '白虎', '螣蛇'],
                 benGua: '101010'
             }
         };
@@ -45,7 +51,23 @@ export default {
             text-align: center;
             font-weight: bold;
             font-size: 18px;
-            letter-spacing: 10px
+            letter-spacing: 10px;
+            margin-bottom: 4px;
+        }
+        .single-row{
+            padding: 5px;
+            height: 20px;
+            line-height: 20px;
+            text-align: center;
+        }
+        .liu-shen{
+            width: 50px;
+            .gua-title{
+                text-align: center;
+                font-weight: bold;
+                font-size: 18px;
+                letter-spacing: 0px
+            }
         }
     }
 </style>
