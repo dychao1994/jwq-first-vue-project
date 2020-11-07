620935fc8633ee70a3d4181e04d480d54988866b
d.y_chao <1179238359@qq.com>
2020-11-07 21:30:16 +0800

    1.完善万年历。

--- src/views/chinaCalendar/ChinaCalendar.vue
+++ src/views/chinaCalendar/ChinaCalendar.vue
@@ -1,54 +1,60 @@
 <template>
     <div class="china-calendar">
-        <div class="calendar-title">
-            <i class="el-icon-back" @click="handleMonth(-1)"></i>
-            <div v-if="!isEdit" class="calendar-title-value" @click="isEdit = true">
-                {{year}}年{{month + 1}}月
-            </div>
-            <el-date-picker
-                v-else
-                v-model="yearAndMonth"
-                type="month"
-                size="small"
-                placeholder="选择月"
-                style="width: 120px;"
-                value-format="yyyy-MM"
-                :clearable="false"
-                @change="isEdit = false"
-            >
-            </el-date-picker>
-            <i class="el-icon-right" @click="handleMonth(1)"></i>
-        </div>
-        <div class="china-calendar-content">
-            <div class="tr-calendar">
-                <div v-for="(item, index) in ['日', '一', '二', '三', '四', '五', '六']" :key="index" class="single-calendar calendar-header">
-                    {{item}}
+        <div class="china-calendar-month">
+            <div class="calendar-title">
+                <i class="el-icon-back" @click="handleMonth(-1)"></i>
+                <div v-if="!isEdit" class="calendar-title-value" @click="isEdit = true">
+                    {{year}}年{{month + 1}}月
                 </div>
-            </div>
-            <div v-for="(outItem, outIndex) in showDayList" :key="outIndex" class="tr-calendar">
-                <div
-                    v-for="(item, index) in outItem"
-                    :key="index"
-                    class="single-calendar"
-                    :class="{'is-current-month': month != item.common.month(), today: isSameDate(item.common), active: selectedDate.common.isSame(item.common)
-                             , holiday: isHoliday(item.common, true), work: isHoliday(item.common, false)}"
-                    @click="handleSelectDate(item)"
+                <el-date-picker
+                    v-else
+                    v-model="yearAndMonth"
+                    type="month"
+                    size="small"
+                    placeholder="选择月"
+                    style="width: 120px;"
+                    value-format="yyyy-MM"
+                    :clearable="false"
+                    @change="isEdit = false"
                 >
-                    <i v-if="isHoliday(item.common, true)" class="iconfont icon-learning-rest holiday-icon" ></i>
-                    <i v-if="isHoliday(item.common, false)" class="iconfont icon-learning-work work-icon" ></i>
-                    <div class="number" :class="{red: isHoliday(item.common, true)}">{{item.common.get('date')}}</div>
-                    <!-- 节气 -->
-                    <div v-if="item.chinaLunar.getJie() || item.chinaLunar.getQi()" class="second-line red">
-                        {{item.chinaLunar.getJie() || item.chinaLunar.getQi()}}
+                </el-date-picker>
+                <i class="el-icon-right" @click="handleMonth(1)"></i>
+            </div>
+            <div class="china-calendar-content">
+                <div class="tr-calendar">
+                    <div v-for="(item, index) in ['日', '一', '二', '三', '四', '五', '六']" :key="index" class="single-calendar calendar-header">
+                        {{item}}
                     </div>
-                    <!-- 节日 -->
-                    <div v-else-if="item.solarLunar.getFestivals().length || item.chinaLunar.getFestivals().length" class="second-line red">
-                        {{item.solarLunar.getFestivals().concat(item.chinaLunar.getFestivals()).join('、')}}
+                </div>
+                <div v-for="(outItem, outIndex) in showDayList" :key="outIndex" class="tr-calendar">
+                    <div
+                        v-for="(item, index) in outItem"
+                        :key="index"
+                        class="single-calendar"
+                        :class="{'is-current-month': month != item.common.month(), today: isSameDate(item.common), active: selectedDate.common.isSame(item.common)
+                                 , holiday: (isHoliday(item.common, true) || ([0, 6].includes(item.common.get('day')))), work: isHoliday(item.common, false)}"
+                        @click="handleSelectDate(item)"
+                    >
+                        <i v-if="isHoliday(item.common, true)" class="iconfont icon-learning-rest holiday-icon" ></i>
+                        <i v-else-if="isHoliday(item.common, false)" class="iconfont icon-learning-work work-icon" ></i>
+                        <span v-else-if="isSameDate(item.common)" class="work-icon">今</span>
+                        <div class="number" :class="{red: (isHoliday(item.common, true) || (!isHoliday(item.common, false) &&[0, 6].includes(item.common.get('day'))))}">{{item.common.get('date')}}</div>
+                        <!-- 节气 -->
+                        <div v-if="item.chinaLunar.getJie() || item.chinaLunar.getQi()" class="second-line red">
+                            {{item.chinaLunar.getJie() || item.chinaLunar.getQi()}}
+                        </div>
+                        <!-- 节日 -->
+                        <div v-else-if="item.solarLunar.getFestivals().length || item.chinaLunar.getFestivals().length" class="second-line red">
+                            {{item.solarLunar.getFestivals().concat(item.chinaLunar.getFestivals()).join('、')}}
+                        </div>
+                        <!-- 农历 -->
+                        <div v-else class="second-line">{{item.chinaLunar.getDayInChinese()}}</div>
                     </div>
-                    <!-- 农历 -->
-                    <div v-else class="second-line">{{item.chinaLunar.getDayInChinese()}}</div>
                 </div>
             </div>
+        </div>
+        <div class="china-calendar-day">
+            <day-calendar :current-day="selectedDate"></day-calendar>
         </div>
     </div>
 </template>
@@ -58,6 +64,9 @@ import dayjs from 'dayjs';
 import {Lunar, Solar, HolidayUtil} from 'lunar-javascript';
 export default {
     name: 'ChinaCalendar',
+    components: {
+        DayCalendar: () => import('./DayCalendar')
+    },
     data() {
         return {
             isEdit: false,
@@ -127,6 +136,7 @@ export default {
             return dayjs().isSame(date, 'date');
         },
         handleSelectDate(item) {
+            this.yearAndMonth = item.common.format('YYYY-MM');
             this.selectedDate = item;
         },
         isHoliday(item, isRest) {
@@ -139,107 +149,129 @@ export default {
 
 <style lang="scss" scoped>
     .china-calendar{
-        .calendar-title{
-            height: 40px;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            .calendar-title-value{
-                cursor: pointer;
-                font-weight: bold;
-                font-size: 20px;
+        border: 4px solid $primary;
+        display: flex;
+        justify-content: space-between;
+        .china-calendar-month{
+            margin: 2px;
+            width: calc(100% - 250px);
+            .calendar-title{
+                height: 40px;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                .calendar-title-value{
+                    cursor: pointer;
+                    font-weight: bold;
+                    font-size: 20px;
+                }
+                i{
+                    cursor: pointer;
+                    &:hover{
+                        color: $primary;
+                    }
+                }
             }
-            i{
-                cursor: pointer;
-                &:hover{
-                    color: $primary;
+            .china-calendar-content{
+
+                .tr-calendar{
+                    display: flex;
+                    flex-wrap: wrap;
+                    justify-content: space-evenly;
+                    align-items: center;
+                    border-top: 1px solid $text-secondary;
+                    /*&:first-child{
+                        border-top: 1px solid $primary;
+                    }*/
+                    /*&:last-child{
+                        border-bottom: 1px solid $text-secondary;
+                    }*/
+                    .single-calendar{
+                        position: relative;
+                        border: 3px solid white;
+                        height: 66px;
+                        width: 14.2%;
+                        text-align: center;
+                        cursor: pointer;
+                        //border-right: 1px solid #333333;
+                        padding: 8px 0 ;
+                        &:first-child{
+                            //border-left: 1px solid #333333;
+                        }
+                        .number{
+                            line-height: 30px;
+                            font-size: 24px;
+                            font-weight: 500;
+
+                            color: $text-primary;
+                        }
+                        .second-line{
+                            line-height: 24px;
+                            font-size: 14px;
+                            color: $text-regular;
+                        }
+                        .red{
+                            color: $red;
+                            font-weight: bold;
+                        }
+                        i {
+                            position: absolute;
+                            font-size: 18px;
+                            color: $red;
+                            left: 4px;
+                            top: 4px;
+                        }
+                        .holiday-icon{
+                            color: $red;
+                        }
+                        .work-icon{
+                            color: $text-primary;
+                        }
+                    }
+                    .is-current-month {
+                        opacity: .5;
+                    }
+                    .today{
+                        background: lighten($primary, 0%);
+                        border-color: $primary;
+                        >div {
+                            color: white !important;
+                        }
+                        .work-icon{
+                            position: absolute;
+                            font-size: 18px;
+                            font-weight: bold;
+                            left: 8px;
+                            top: 8px;
+                            color: white !important;
+                        }
+                    }
+                    .active {
+                        border-color: $primary !important;
+                    }
+                    .holiday {
+                        $color: lighten($red, 40%);
+                        background: $color;
+                        border-color: $color;
+                    }
+                    .work{
+                        $color: lighten($text-placeholder, 10%);
+                        background: $color;
+                        border-color: $color;
+                    }
+                    .calendar-header{
+                        height: 32px;
+                        text-align: center;
+                        padding: 8px 0;
+                        font-size: 16px;
+                        font-weight: bold;
+                    }
                 }
             }
         }
-        .china-calendar-content{
-
-            .tr-calendar{
-                display: flex;
-                flex-wrap: wrap;
-                justify-content: space-evenly;
-                align-items: center;
-                border-top: 1px solid $text-secondary;
-                &:last-child{
-                    border-bottom: 1px solid $text-secondary;
-                }
-                .single-calendar{
-                    position: relative;
-                    border: 3px solid white;
-                    height: 66px;
-                    width: 14.2%;
-                    text-align: center;
-                    cursor: pointer;
-                    //border-right: 1px solid #333333;
-                    padding: 8px 0 ;
-                    &:first-child{
-                        //border-left: 1px solid #333333;
-                    }
-                    .number{
-                        line-height: 30px;
-                        font-size: 24px;
-                        font-weight: 500;
-
-                        color: $text-primary;
-                    }
-                    .second-line{
-                        line-height: 24px;
-                        font-size: 14px;
-                        color: $text-regular;
-                    }
-                    .red{
-                        color: $red;
-                        font-weight: bold;
-                    }
-                    i {
-                        position: absolute;
-                        font-size: 18px;
-                        color: $red;
-                        left: 4px;
-                        top: 4px;
-                    }
-                    .holiday-icon{
-                        color: $red;
-                    }
-                    .work-icon{
-                        color: $text-primary;
-                    }
-                }
-                .is-current-month {
-                    opacity: .5;
-                }
-                .today{
-                    background: lighten($primary, 0%);
-                    border-color: $primary;
-                    >div {
-                        color: white !important;
-                    }
-                }
-                .active {
-                    border-color: $primary !important;
-                }
-                .holiday {
-                    $color: lighten($red, 40%);
-                    background: $color;
-                    border-color: $color;
-                }
-                .work{
-                    $color: lighten($text-placeholder, 10%);
-                    background: $color;
-                    border-color: $color;
-                }
-                .calendar-header{
-                    height: 32px;
-                    text-align: center;
-                    padding: 8px 0;
-                    font-size: 16px;
-                    font-weight: bold;
-                }
-            }
+        .china-calendar-day{
+            width: 240px;
+            //background: lighten($primary, 40%);
         }
     }
 </style>
---
+++ src/views/chinaCalendar/DayCalendar.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="day-calendar">
+        <div class="day-calendar-header">
+            {{currentDay.common.format('YYYY年MM月DD日 dddd')}}
+        </div>
+        <div class="day-calendar-day">
+            <div class="show-day">{{currentDay.common.format('DD')}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DayCalendar',
+    props: {
+        currentDay: {
+            type: Object,
+            default: () => { return {}; }
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+    .day-calendar{
+        height: 100%;
+        background: $primary;
+        .day-calendar-header{
+            text-align: center;
+            line-height: 40px;
+            color: white;
+            font-weight: bold;
+        }
+        .day-calendar-day{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            .show-day{
+                $width: 80px;
+                width: $width;
+                height: $width;
+                border-radius: 10px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+
+                font-size: 40px;
+                font-weight: bold;
+                color: white;
+                background: lighten($yellow, 5%);
+            }
+        }
+    }
+</style>
